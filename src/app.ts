387c7fef22cab58cec5a2ea15b182bca14713a74
Tyler Schum <tschumdev@gmail.com>
2020-04-15 21:20:30 -0400

    add OPTIONS to allowed methods

--- src/app.ts
+++ src/app.ts
@@ -19,9 +19,9 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Client'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
